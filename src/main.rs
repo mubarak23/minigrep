bf387f5d21f58f219315dacac56855b76e1dbe68
mubarak23 <mubarakaminu340@gmail.com>
2024-04-15 13:57:22 +0100

    read from a file and print the content

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // we use collect to turn the iterator into a vector containing all the values produced by the iterator
-    dbg!(args);
+    dbg!(&args);
+
+    let query = &args[1]; // reference the first argument pass via the CL
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", &file_path);
+
+    // reading from a file 
+    let contents = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
   
 }
