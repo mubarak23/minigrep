a3dde1413229299979ed25db1d5d670a6cbc3f7b
mubarak23 <mubarakaminu340@gmail.com>
2024-04-16 11:57:19 +0100

    implement Config struct and new function

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,42 @@
 use std::env;
 use std::fs;
 
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect(); // we use collect to turn the iterator into a vector containing all the values produced by the iterator
     dbg!(&args);
 
-    let query = &args[1]; // reference the first argument pass via the CL
-    let file_path = &args[2];
+    let config = Config::new(&args);
+    // parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", &file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
     // reading from a file 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
     .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
   
 }
+
+
+
+// fn parse_config(args: &[String]) -> Config {
+//      let query = args[1].clone(); // reference the first argument pass via the CL
+//     let file_path = args[2].clone();
+//     Config {query, file_path}
+// }
